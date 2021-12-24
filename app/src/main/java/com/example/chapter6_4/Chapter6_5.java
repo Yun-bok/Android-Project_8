9b55352c8d7e58ed09aebba577f1f6ef3c09a7e6
[Yun-bok] <[gkdldbsqhr@naver.com]>
2021-12-24 19:36:12 +0900

    How to use Relative Layout?

---
+++ app/src/main/java/com/example/chapter6_4/Chapter6_5.java
@@ -0,0 +1,14 @@
+package com.example.chapter6_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chapter6_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter6_5);
+    }
+}
---
+++ app/src/main/java/com/example/chapter6_4/Practice6_1.java
@@ -0,0 +1,14 @@
+package com.example.chapter6_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Practice6_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice6_1);
+    }
+}
